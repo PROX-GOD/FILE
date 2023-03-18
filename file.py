c7d0f9cb88d6be42d32dd1a360fb9f91dba7412e
PROX-GOD <117366207+PROX-GOD@users.noreply.github.com>
2023-03-18 20:30:46 +0545

    Add files via upload

---
+++ file.py
@@ -0,0 +1,12 @@
+mport os, platform
+os.system('git pull')
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import file64
+elif bit == '32bit':
+    import file
